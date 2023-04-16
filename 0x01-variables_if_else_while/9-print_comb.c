00b958a219e9187c4d0f43eac5cdedef63cf56da
k1syd <sefahyeboah5@gmail.com>
2023-04-15 17:59:24 -0700

    10th

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,12 @@
 int main(void)
 {
 	int i, com = 44, spe = 32;
-	for (i = 48; i <= 57; i++) {
+	
+	for (i = 48; i <= 57; i++)
+	{
 		putchar(i);
-		if (i != 57) {
+		if (i != 57)
+		{
 			putchar(com);
 			putchar(spe);
 		}
